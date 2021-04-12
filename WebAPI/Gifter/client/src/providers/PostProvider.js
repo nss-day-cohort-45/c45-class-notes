2a97d62be2e838dbad93f6369eb3cb766fd5beb7
Madi Peper <madisonpeper@gmail.com>
2021-04-12 13:43:58 -0500

    Added Gifter project to WebAPI directory; Added React Client for Gifter API;

---
+++ WebAPI/Gifter/client/src/providers/PostProvider.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export const PostContext = React.createContext();
+
+export const PostProvider = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const getAllPosts = () => {
+    // the proxy that was set up in package.json will be handling the first part of the URL
+    return fetch("/api/post")
+      .then((res) => res.json())
+      .then(setPosts);
+  };
+
+  const addPost = (post) => {
+    return fetch("/api/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(post),
+    });
+  };
+
+  return (
+    <PostContext.Provider value={{ posts, getAllPosts, addPost }}>
+      {props.children}
+    </PostContext.Provider>
+  );
+};
