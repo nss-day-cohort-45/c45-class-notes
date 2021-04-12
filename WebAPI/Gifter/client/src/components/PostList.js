0fdde40064891c925b6354754cbf994250ab7583
Andy Collins <askingalot@gmail.com>
2021-04-12 14:20:27 -0500

    fixing markup

--- WebAPI/Gifter/client/src/components/PostList.js
+++ WebAPI/Gifter/client/src/components/PostList.js
@@ -3,19 +3,23 @@ import { PostContext } from "../providers/PostProvider";
 import Post from "./Post";
 
 const PostList = () => {
-    const { posts, getAllPosts } = useContext(PostContext);
+  const { posts, getAllPosts } = useContext(PostContext);
 
-    useEffect(() => {
-        getAllPosts();
-    }, []);
+  useEffect(() => {
+    getAllPosts();
+  }, []);
 
-    return (
-        <div>
-            {posts.map((post) => (
-                <Post  key={post.id} post={post} />
-            ))}
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="cards-column">
+          {posts.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PostList;
